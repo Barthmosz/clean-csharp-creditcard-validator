854baafb1eea0a0af20201693f0315064baf631f
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-10 23:24:00 -0300

    feat: ensure CreditCardValidator works properly

---
+++ Clean.CreditCardValidator/CreditCardValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Clean.CreditCardValidator
+{
+    public static class CreditCardValidator
+    {
+        public static bool Validate(string creditCard)
+        {
+            string creditCardRegex = "^\\d{16}";
+            return Regex.IsMatch(creditCard, creditCardRegex);
+        }
+    }
+}
