269cde117bbec93700b3a3f3d2a8de0bc0bd21a7
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-10 23:36:19 -0300

    test: ensure CreditCardValidator returns false if an empty credit card is provided

--- Clean.CreditCardValidator.Test/CreditCardValidatorTest.cs
+++ Clean.CreditCardValidator.Test/CreditCardValidatorTest.cs
@@ -10,5 +10,11 @@ namespace Clean.CreditCardValidator.Test
         {
             Assert.That(CreditCardValidator.Validate("1234 1234 1234"), Is.EqualTo(false));
         }
+
+        [Test, Description("should return false if an empty credit card is provided")]
+        public void CreditCardValidator_EmptyCreditCard_ReturnsFalse()
+        {
+            Assert.That(CreditCardValidator.Validate(""), Is.EqualTo(false));
+        }
     }
 }
