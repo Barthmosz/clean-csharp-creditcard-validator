ca8fbcbb6292b7d3cef46888205bcb4f2a75e97d
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-10 23:34:13 -0300

    test: ensure CreditCardValidator returns false if an invalid credit card is provided

---
+++ Clean.CreditCardValidator.Test/CreditCardValidatorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Clean.CreditCardValidator.Test
+{
+    [TestFixture]
+    public class CreditCardValidatorTest
+    {
+        [Test, Description("should return false if an invalid credit card is provided")]
+        public void CreditCardValidator_InvalidCreditCard_ReturnsFalse()
+        {
+            Assert.That(CreditCardValidator.Validate("1234 1234 1234"), Is.EqualTo(false));
+        }
+    }
+}
